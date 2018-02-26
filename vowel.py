b4abff6396279ee20ffcba60cc72d51496c38299
pattumehala <35220817+pattumehala@users.noreply.github.com>
2018-02-26 16:59:14 +0530

    vowels.py
    vowels or not

---
+++ vowel.py
@@ -0,0 +1,10 @@
+a=raw_input()
+n=len(a)
+c=0
+for i in range(0,n):
+  if(a[i]=='a' or a[i]=='e' or a[i]=='i' or a[i]=='o' or a[i]=='u'):
+    C+=1
+if(p>=1):  
+    print("yes")
+else:
+   print("no")
