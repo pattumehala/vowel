7ec4f1b8c342d8612c4ebd011e0a4a050e8242ef
pattumehala <35220817+pattumehala@users.noreply.github.com>
2018-02-28 15:44:55 +0530

    vowels.py
    the string has vowels or not

--- vowel.py
+++ vowel.py
@@ -2,9 +2,9 @@ a=raw_input()
 n=len(a)
 c=0
 for i in range(0,n):
-  if(a[i]=='a' or a[i]=='e' or a[i]=='i' or a[i]=='o' or a[i]=='u'):
-    C+=1
-if(p>=1):  
+  if(a[i]=='a' or 'e' or 'i' or 'o' or 'u'):
+    c+=1
+if(c>=1):  
     print("yes")
 else:
    print("no")
